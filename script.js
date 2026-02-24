40ee89f2132818de102abb699e49996bcb5f5684
satyam1124 <70072699+satyam1124@users.noreply.github.com>
2026-02-25 02:17:23 +0530

    Create script.js with extracted JavaScript code from index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript code extracted from index.html
+
+// Example JavaScript code
+function exampleFunction() {
+    console.log('This is an example function.');
+}
+
+// Add more JavaScript code here...
